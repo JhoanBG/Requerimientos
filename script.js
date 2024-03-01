c6b1ecbb0232ff155c2b705613e366c84af51dcd
JhoanBG <161781817+JhoanBG@users.noreply.github.com>
2024-02-29 19:10:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+alert("Bienvenidos a Software de automatización de pedidos para el restaurante de comidas rapidas Comic Hamburguer");
+alert("Proyecto formativo de Karol Valenciano, Carlos Santos y Jhoan Bolaños");
